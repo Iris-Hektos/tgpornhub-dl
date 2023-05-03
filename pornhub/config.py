c473aa771d481b0a0d2d7393071d26f706a6297c
Iris <104681041+Iris-Hektos@users.noreply.github.com>
2023-05-03 10:34:05 +0200

    config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -1,11 +1,11 @@
 from typing import List
 
-API_ID: int = insert_your_own_api_id_here
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
+API_ID: int = 7305705
+API_HASH: str = "d6f6f56e9d58a495dcec9008e415f79e"
+TOKEN: str = "6006679710:AAFWAnjakINuUEID1GTHk5l_WE_GeT42EUc"
 
-log_chat: int = insert_log_chat_id_here
-sub_chat: str = "insert_channel_username_here"
+log_chat: int = -928387807
+sub_chat: str = "yukiohub"
 sudoers: List[int] = [1757169682, 859229457]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
